557891d9bcc38c3a01cbbd7f72b8bf0d18c339bd
monicawinter <m.m.winter@hotmail.com>
2016-04-04 16:22:01 -0400

    added beginning of transition

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var $svg = $('svg');
+var $lines = $('.lines');
+var $btn = $('.btn');
+var $engage = $('.engage');
+
+$btn.on('click', function () {
+  $lines.addClass('line2');
+  $lines.removeClass('lines');
+  $engage.addClass('is-engaged');
+});
